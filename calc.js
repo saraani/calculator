e252a3273c9cc88ea3f85d92e0c4d1949ea2873c
saraani <ummulsaraani@gmail.com>
2022-06-30 23:17:08 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,24 @@
+//1. display number in text box
+function displayNum(num){
+result.value+=num
+}
+
+//2. clear text box
+
+function clearBox(){
+    result.value=""
+}
+
+//3. evaluate expression
+
+function evaluateExpr(){
+    result.value = eval(result.value)
+
+}
+
+//4. remove last item from text box
+
+function removeItem(){
+    curexpr = result.value
+    result.value = curexpr.slice(0,-1)
+}
